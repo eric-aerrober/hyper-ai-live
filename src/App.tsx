9b7f04b57e698fe142a4c505a134ce1856647d1c
soul <opensource@aerrobert.com>
2024-04-19 18:17:16 -0700

    Fri Apr 19 18:17:16 PDT 2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { WorkflowDisplayed } from "pages/workflow"
+import { useState } from "react"
 import { knownWorkflows } from "workflows"
 
 export function App () {
 
-    const page = window.location.pathname.split('/').pop()
+    const [page, setPage] = useState<string | null>(null)
 
     return <>
         <div
             className="w-full h-8 bg-indigo-600 text-white flex gap-5 px-5 py-1"
         >
-            <a href="/generateIdea" className="px-2 rounded text-white hover:bg-indigo-300">Generate Idea</a>
-            <a href="/refineIdea" className="px-2 rounded text-white hover:bg-indigo-300">Refine Idea</a>
+            <a 
+                onClick={() => setPage('generateIdea')}
+                className="px-2 rounded text-white hover:bg-indigo-300">Generate Idea</a>
+            <a  
+                onClick={() => setPage('refineIdea')}
+                className="px-2 rounded text-white hover:bg-indigo-300">Refine Idea</a>
         </div>
 
         <WorkflowDisplayed workflow={knownWorkflows[page || 'generateIdea']} />
